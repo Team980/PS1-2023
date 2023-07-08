7db59a1fa49331e1fbeb01cc76f7813cb67f4abb
Davis <wariokirby@gmail.com>
2023-07-08 01:21:27 -0700

    added and commented out the encoder multipliers

--- src/main/java/frc/robot/subsystems/SwervePod.java
+++ src/main/java/frc/robot/subsystems/SwervePod.java
@@ -25,15 +25,26 @@ public class SwervePod extends SubsystemBase {
     swerveMotor = new CANSparkMax(swerveID, MotorType.kBrushless);
     dirEnc = new CANCoder(dirID);
     velEnc = driveMotor.getEncoder();
+    //uncomment when switching to real units instead of rotations and rpm
+    //velEnc.setPositionConversionFactor(((4/12) * Math.PI) / (8.14)); //circumference for 4" wheel divided by 12" to a foot / gear ratio * -> feet
+    //velEnc.setVelocityConversionFactor(((4/12) * Math.PI) / (8.14 * 60));//circumference for 4" wheel divided by 12" to a foot / gear ratio * convert to seconds -> feet per second
 
     podID = driveID;
   }
 
   @Override
   public void periodic() {
-    SmartDashboard.putNumber("Speed" + podID, velEnc.getVelocity());
-    SmartDashboard.putNumber("Distance" + podID, velEnc.getPosition());
+    SmartDashboard.putNumber("Speed" + podID, getSpeed());
+    SmartDashboard.putNumber("Distance" + podID, getDistance());
     // This method will be called once per scheduler run
+  }
+
+  public double getSpeed(){
+    return velEnc.getVelocity();
+  }
+
+  public double getDistance(){
+    return velEnc.getPosition();
   }
 
   
